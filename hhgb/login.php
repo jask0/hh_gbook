ee3f5198234bdb5585c5bb5185e9f79b2bec7f75
jask0 <jasmin.music@gmx.com>
2016-04-25 15:27:48 +0200

    fix 404 error

--- hhgb/login.php
+++ hhgb/login.php
@@ -40,7 +40,8 @@ if($_POST){
 <html>
 <head>
 <title>Login</title>
-<?php loadMeta(); ?>
+<!-- Font-Awesome CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <!-- Extern Bootstrap CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
