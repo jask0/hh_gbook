a6e506ff71d477aba6b1f1f7fb8654dd9ad1e846
jask0 <jasmin.music@gmx.com>
2016-12-13 01:17:30 +0100

    add 3 escape functions

--- hhgb/conf/functions.php
+++ hhgb/conf/functions.php
@@ -88,3 +88,46 @@ function testPosition($pos){
 		return "hhgb/";
 	}
 }
+
+function escape_data ($data, $c=NULL) {
+
+if (function_exists('mysqli_real_escape_string')) {
+	$data = mysqli_real_escape_string ($c, trim($data));
+	$data = strip_tags($data);
+} else {
+	$data = mysqli_escape_string (trim($data));
+	$data = strip_tags($data);
+}
+return $data;
+
+}
+
+function escape_input($data, $c=NULL, $min=2, $max=255){
+	if (preg_match('%^[A-Za-zöäüßÄÖÜ\.\' \-]{'.$min.','.$max.'}$%', stripslashes(trim($data)))) {
+		$nd =  escape_data($data, $c);
+	} else {
+		$nd = FALSE;
+	}
+	
+	return $nd;
+}
+
+function escape_email($email, $c=NULL){
+	if (preg_match ('%^[A-Za-z0-9._\%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$%', stripslashes(trim($email)))) {
+		$e = escape_data($email, $c);
+	} else {
+		$e = FALSE;
+	}
+	
+	return $e;
+}
+
+function escape_url($url, $c=NULL){
+	if (preg_match ('@^(?:http://)?([^/]+)@i', stripslashes(trim($url)))) {
+		$e = escape_data($url, $c);
+	} else {
+		$e = FALSE;
+	}
+	
+	return $e;
+}
